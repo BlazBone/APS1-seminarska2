cb5dd79fc5e721c5460bd4b9a9a3e5a398862c0d
BlazBone <bb6846@student.uni-lj.si>
2021-12-10 22:43:49 +0100

    dela da isce pot z najmanj prestopanji i hope

--- naloga7/Naloga7.java
+++ naloga7/Naloga7.java
@@ -7,52 +7,36 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Queue;
 
 public class Naloga7 {
-    public static class NodePostaja {
-        int idPosaje;
-        int idLinije;
-        ArrayList<NodePostaja> sosedje;
-        ArrayList<Integer> idSosedov;
 
-        public NodePostaja(int idPostaje, int idLinije) {
-            this.idPosaje = idPostaje;
-            this.idLinije = idLinije;
-            this.sosedje = new ArrayList<>();
-            this.idSosedov = new ArrayList<>();
+    public static class Linija {
+        int idLinije;
+        HashSet<Integer> sosedneLinije;
+        HashSet<Integer> postaje;
+
+        public Linija(int idlinije, HashSet<Integer> sosedi, HashSet<Integer> postaje) {
+            this.idLinije = idlinije;
+            this.sosedneLinije = sosedi;
+            this.postaje = postaje;
         }
 
-    }
-
-    public static class Tuple {
-        int idPostaje;
-        int stKorakov;
-        int idLinije;
-
-        public Tuple(int id, int st, int linija) {
-            this.idPostaje = id;
-            this.stKorakov = st;
-            this.idLinije = linija;
+        public void izpisi() {
+            System.out.printf("Linija: %d\n sosednjeLinije: %s\n postaje: %s\n", this.idLinije,
+                    this.sosedneLinije.toString(), this.postaje.toString());
         }
-
     }
 
-    public static class NodeLinija {
+    public static class QueueEl {
         int idLinije;
-        ArrayList<NodeLinija> sosedje;
-        ArrayList<Integer> idSosedov;
-        ArrayList<Integer> idPostaj;
+        int korak;
 
-        public NodeLinija(int idPostaje, int idLinije) {
-
-            this.idLinije = idLinije;
-            this.idPostaj = new ArrayList<>();
-            this.sosedje = new ArrayList<>();
-            this.idSosedov = new ArrayList<>();
+        public QueueEl(int id, int korak) {
+            this.idLinije = id;
+            this.korak = korak;
         }
-
     }
-
     // imamo dva nacina imamo en graf ki ima vse postaje s tem iscemo najjkrajso pot
     // imamo gra fki ima samo postaje s tem iscemo pot znajmanj prestopanji ravno
     // taki, da iscemo najkraso pot le da se tokrat sprehajamo samo med linijami
@@ -66,94 +50,65 @@ public class Naloga7 {
 
         int idZacetka, idKonca;
 
-        String[][] linije = new String[stLinij][];
-        NodePostaja prva;
-        NodePostaja zacetna;
+        Linija[] linije = new Linija[stLinij];
+
         String[] temp;
-        // boolean[][] povezavePostajalisc = new boolean[][];
         int steviloPostaj = 0;
+
         for (int i = 0; i < stLinij; i++) {
 
             temp = tok.readLine().split(",");
+            HashSet<Integer> tempPostaje = new HashSet<>();
+            HashSet<Integer> tempSosedi = new HashSet<>();
 
-            for (String a : temp) {
-                if (Integer.parseInt(a) > steviloPostaj) {
-                    steviloPostaj = Integer.parseInt(a);
+            for (String postaja : temp) {
+                tempPostaje.add(Integer.parseInt(postaja));
+            }
+            for (int j = 0; j < i; j++) {
+                for (Integer postajaInteger : tempPostaje) {
+                    if (linije[j].postaje.contains(postajaInteger)) {
+                        tempSosedi.add(j);
+                        linije[j].sosedneLinije.add(i);
+                        break;
+                    }
                 }
             }
 
-            linije[i] = temp;
+            linije[i] = new Linija(i, tempSosedi, tempPostaje);
 
         }
         temp = tok.readLine().split(",");
         idZacetka = Integer.parseInt(temp[0]);
         idKonca = Integer.parseInt(temp[1]);
 
-        // System.out.println(Arrays.deepToString(linije));
-        // System.out.println(steviloPostaj);
+        Queue<QueueEl> q = new LinkedList<>();
+        HashSet<Integer> smoZeObiskaliLinijo = new HashSet<>();
 
-        boolean[][] seznamPovezavPostaj = new boolean[steviloPostaj][steviloPostaj];
-        // System.out.println(Arrays.deepToString(seznamPovezavPostaj));
-        int stlinije = 0;
-        Tuple[] vsePostaje = new Tuple[steviloPostaj];
-        for (String[] linija : linije) {
-
-            for (int i = 0; i < linija.length; i++) {
-
-                if (vsePostaje[Integer.parseInt(linija[i]) - 1] == null) {
-                    vsePostaje[Integer.parseInt(linija[i]) - 1] = new Tuple(Integer.parseInt(linija[i]), 0, stlinije);
-                } else {
-
-                }
-                if (i == 0) {
-                    continue;
-                }
-                seznamPovezavPostaj[Integer.parseInt(linija[i]) - 1][Integer.parseInt(linija[i - 1]) - 1] = true;
-                seznamPovezavPostaj[Integer.parseInt(linija[i - 1]) - 1][Integer.parseInt(linija[i]) - 1] = true;
+        for (Linija linija : linije) {
+            linija.izpisi();
+            System.out.println();
+            if (linija.postaje.contains(idZacetka)) {
+                q.add(new QueueEl(linija.idLinije, 0));
             }
-            stlinije++;
-
         }
-        // System.out.println(Arrays.deepToString(seznamPovezavPostaj));
-        // System.out.println(Arrays.deepToString(vsePostaje));
-        for (Tuple tuple : vsePostaje) {
-            System.out.print(tuple.idPostaje);
-            System.out.print(" ");
-            System.out.println(tuple.idLinije);
-        }
+        QueueEl tempQel = null;
+        while (!q.isEmpty()) {
+            tempQel = q.poll();
 
-        // addi v q zacetno pozicijo in korak ima 0
-        // while q ni empty gres
-        // vsakic popnemo prvi element damo njegove sosede v vrsto in jim damo korak+1.
-        // potem
-        // ko pridemo do zeljenega vozlisca se sutavimo in vsakic ko pridemo do novega
-        // si zapisemo linijio, da drzimo stevilo linij
-        LinkedList<Tuple> vrstica = new LinkedList<>();
-        int dolzinaPoti = 0;
-        vrstica.add(vsePostaje[idZacetka - 1]);
-        LinkedList<Integer> smoZe = new LinkedList<>();
-        HashSet<Integer> obiskaneLinije = new HashSet<>();
-        Tuple tempTuple = null;
-        while (!vrstica.isEmpty()) {
-            tempTuple = vrstica.removeFirst();
-            System.out.printf("      %d %d \n", tempTuple.idPostaje, tempTuple.stKorakov);
-
-            if (tempTuple.idPostaje == idKonca) {
+            if (linije[tempQel.idLinije].postaje.contains(idKonca)) {
                 break;
             }
-            smoZe.add(tempTuple.idPostaje);
-            obiskaneLinije.add(tempTuple.idLinije);
-            for (int i = 0; i < steviloPostaj; i++) {
-                if (seznamPovezavPostaj[tempTuple.idPostaje - 1][i] && !(smoZe.contains(i + 1))) {
-                    vrstica.add(new Tuple(i + 1, tempTuple.stKorakov + 1, vsePostaje[i].idLinije));
+            smoZeObiskaliLinijo.add(tempQel.idLinije);
+
+            for (Integer idLinije : linije[tempQel.idLinije].sosedneLinije) {
+                if (!smoZeObiskaliLinijo.contains(idLinije)) {
+                    q.add(new QueueEl(idLinije, tempQel.korak + 1));
                 }
             }
+
         }
-        System.out.println();
-        System.out.println();
-        System.out.println(tempTuple.idLinije);
-        System.out.println(tempTuple.idPostaje);
-        System.out.println(tempTuple.stKorakov);
+        System.out.println(tempQel.idLinije);
+        System.out.println(tempQel.korak);
 
         p.close();
         tok.close();
