ed6a60b6137ee8a5ac40eb6c3e2afc7596271ef4
BlazBone <bb6846@student.uni-lj.si>
2021-12-25 18:18:37 +0100

    7 kinda

--- naloga10/Naloga10.java
+++ naloga10/Naloga10.java
@@ -2,7 +2,11 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -15,36 +19,71 @@ public class Naloga10 {
         private int idTocke;
         private double x;
         private double y;
+        private Tocka najblizja;
+        private double razdaljaDoNajblizje;
+        private Mnozica spada;
+        private int tuki;
 
         public Tocka(String[] a, int id) {
             this.x = Double.parseDouble(a[0]);
             this.y = Double.parseDouble(a[1]);
             this.idTocke = id;
+            this.najblizja = null;
+            this.razdaljaDoNajblizje = Double.MAX_VALUE;
+            this.spada = null;
+            this.tuki = id;
         }
 
         public void izpisi() {
-            System.out.printf("Tocka %d: %f %f\n", this.idTocke, this.x, this.y);
+            System.out.printf("Tocka %d: %f %f raz: %f naj: %d spada:%d tuki:%d\n", this.idTocke, this.x, this.y,
+                    this.razdaljaDoNajblizje, this.najblizja.idTocke, this.spada.idMnozice, this.tuki);
         }
 
         public double razdalja(Tocka dva) {
-            return Math.sqrt(Math.pow((this.x - dva.x), 2) + Math.pow((this.x - dva.x), 2));
+            return Math.sqrt(Math.pow((this.x - dva.x), 2) + Math.pow((this.y - dva.y), 2));
+        }
+
+        public void zamenjajMnozico(Mnozica a) {
+            this.spada = a;
         }
 
     }
 
     public static class Mnozica {
         private int idMnozice;
-        private ArrayList<Tocka> tocke;
-        private HashMap<Mnozica, Double> povezaveInRazdalje;
+        private HashSet<Tocka> tocke;
+        // private HashMap<Mnozica, Double> povezaveInRazdalje;
 
         public Mnozica(int id) {
-            this.tocke = new ArrayList<>();
-            this.povezaveInRazdalje = new HashMap<>();
+            this.tocke = new HashSet<>();
+            // this.povezaveInRazdalje = new HashMap<>();
             this.idMnozice = id;
         }
 
         public void dodajTocko(Tocka a) {
             this.tocke.add(a);
+            a.zamenjajMnozico(this);
+            if (a.idTocke < this.idMnozice) {
+                idMnozice = a.idTocke;
+            }
+        }
+
+        public void dodajMnozico(Mnozica a) {
+            for (Tocka tocka : a.tocke) {
+                this.dodajTocko(tocka);
+
+            }
+        }
+
+        public void izpisiMnozico() {
+
+            System.out.println("MNOZICA: " + this.idMnozice);
+            for (Tocka tocka : tocke) {
+                tocka.izpisi();
+            }
+
+            System.out.println();
+
         }
 
     }
@@ -55,26 +94,83 @@ public class Naloga10 {
         PrintWriter p = new PrintWriter(new FileWriter(args[1]));
 
         int stTock = Integer.parseInt(tok.readLine());
+        double[][] razdaljeMedTockami = new double[stTock][stTock];
 
         Tocka[] vseTocke = new Tocka[stTock];
+        ArrayList<Tocka> vse = new ArrayList<>();
         ArrayList<Mnozica> vseMnozice = new ArrayList<>();
         for (int i = 0; i < stTock; i++) {
-            vseTocke[i] = new Tocka(tok.readLine().split(","), i + 1);
-            vseMnozice.add(new Mnozica(i + 1));
-            vseMnozice.get(i).dodajTocko(vseTocke[i]);
-
+            vse.add(new Tocka(tok.readLine().split(","), i + 1));
+            vseMnozice.add(new Mnozica(vse.get(i).idTocke));
+            vseMnozice.get(i).dodajTocko(vse.get(i));
             for (int j = 0; j < i; j++) {
-                double razdalja = vseTocke[i].razdalja(vseTocke[j]);
-                vseMnozice.get(i).povezaveInRazdalje.put(vseMnozice.get(j), razdalja);
-                vseMnozice.get(j).povezaveInRazdalje.put(vseMnozice.get(j), razdalja);
+                double raz = vse.get(i).razdalja(vse.get(j));
+
+                if (raz < vse.get(i).razdaljaDoNajblizje) {
+                    vse.get(i).razdaljaDoNajblizje = raz;
+                    vse.get(i).najblizja = vse.get(j);
+                }
+                if (raz < vse.get(j).razdaljaDoNajblizje) {
+                    vse.get(j).razdaljaDoNajblizje = raz;
+                    vse.get(j).najblizja = vse.get(i);
+                }
+
             }
 
         }
         int stSkupin = Integer.parseInt(tok.readLine());
         tok.close();
 
-        for (Tocka tocka : vseTocke) {
-            tocka.izpisi();
+        Collections.sort(vse, (o1, o2) -> Double.compare(o1.razdaljaDoNajblizje, o2.razdaljaDoNajblizje));
+
+        while (stSkupin >= ) {
+            
         }
+
+        // for (int i = 0; i < vse.size() - stSkupin + 1; i++) {
+        //     Tocka tc = vse.get(i);
+        //     Mnozica a = tc.najblizja.spada;
+        //     Mnozica b = tc.spada;
+
+        //     tc.tuki = tc.najblizja.tuki;
+
+        //     // if (!a.equals(b)) {
+        //     // System.out.println("DODAMO");
+        //     // vse.get(i).spada.izpisiMnozico();
+        //     // System.out.println("+");
+
+        //     // vse.get(i).najblizja.spada.izpisiMnozico();
+
+        //     // vse.get(i).spada.dodajMnozico(vse.get(i).najblizja.spada);
+        //     // System.out.println("=");
+        //     // vse.get(i).spada.izpisiMnozico();
+
+        //     // System.out.println("ODSTRANIMO");
+        //     // a.izpisiMnozico();
+        //     // vseMnozice.remove(a);
+
+        //     // System.out.println();
+        //     // System.out.println();
+        //     // System.out.println();
+        //     // }
+
+        //     if (vseMnozice.size() == stSkupin) {
+        //         break;
+        //     }
+        // }
+
+        System.out.println();
+        System.err.println(vse.size());
+        // System.out.println(vseMnozice.size());
+        for (Tocka jasnTocka : vse) {
+            jasnTocka.izpisi();
+        }
+        System.out.println();
+
+        System.out.println(vseMnozice.size());
+        for (Mnozica mnozica : vseMnozice) {
+            mnozica.izpisiMnozico();
+        }
+
     }
 }
